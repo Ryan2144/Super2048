0bd7d6661b2c76b36e929e58a778576662cb609e
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2024-09-12 19:43:47 -0500

    Win message no longer will appear, winning is now by getting an S-rank

--- js/game_manager.js
+++ js/game_manager.js
@@ -182,7 +182,7 @@ GameManager.prototype.move = function (direction) {
           if (merged.value > self.rankScore) self.rankScore = merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === tileGoal) self.won = true;
+          if (merged.value === tileGoal) self.won = false;
         } else {
           self.moveTile(tile, positions.farthest);
         }
